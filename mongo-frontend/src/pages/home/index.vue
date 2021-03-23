16a0811bd0c3beb754862667e10e429388511c3b
wuquanru <18242086605@163.com>
2021-03-23 17:33:18 +0800

    no message

---
+++ mongo-frontend/src/pages/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="container">
+		<view>
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
