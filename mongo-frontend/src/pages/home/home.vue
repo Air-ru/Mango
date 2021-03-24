2e5a816608b180299b8c392ce1d2b7439827d881
Quanru <18242086605@163.com>
2021-03-24 14:17:57 +0800

    no message

---
+++ mongo-frontend/src/pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+    <view class="container">
+        <!-- <uni-rate :size="18" :value="5" /> -->
+    </view>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {};
+    },
+    onLoad() {},
+    methods: {},
+};
+</script>
+
+<style>
+</style>
